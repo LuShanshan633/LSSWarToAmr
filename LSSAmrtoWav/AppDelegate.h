6630f144f07fb00397a72239b3ec9f208810929b
陆闪闪 <gmyuan11@gmail.com>
2020-07-08 16:08:00 +0800

    no message

---
+++ LSSAmrtoWav/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LSSAmrtoWav
+//
+//  Created by yingju on 2020/7/8.
+//  Copyright © 2020 Lu Shanshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
